e9490ce82e0b542865c96b5d410606c6966efc3d
stevanlalovic <stevan.lalovic1999@gmail.com>
2022-03-31 11:14:10 +0200

    4th commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ import Transition from './Transition';
 function Contact() {
     const contact = gsap.timeline();
     const contacth1 = useRef(null);
+    const contacth2 = useRef(null);
     const contactimg = useRef(null);
     useEffect(() => {
         contact.from(contacth1.current, {
@@ -13,6 +14,12 @@ function Contact() {
             x: -400,
             opacity: 0
         },"-=3.5")
+        contact.from(contacth2.current, {
+            duration: .9,
+            skewX: 10,
+            x: -400,
+            opacity: 0
+        },"-=2")
         contact.from(contactimg.current, {
             duration: .3,
             y: -200,
@@ -25,6 +32,7 @@ function Contact() {
             <div className="contact-image contact-overlay" ref={contactimg}></div>
             <div className="container-contact">
                 <h1 ref={contacth1}>contact Page</h1>
+                <h2 ref={contacth2}>stevan.code@gmail.com</h2>
             </div>
         </div>
     )
