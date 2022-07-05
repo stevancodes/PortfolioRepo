02a82d0f23700192f7e9e161aa3408c901c71a91
stevanlalovic <stevan.lalovic1999@gmail.com>
2022-07-05 15:35:09 +0200

    rel noreferrer

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,12 +31,12 @@ function Projects() {
       <Transition timeline={projects} />
       <div className="project-image project-overlay" ref={projectimg}></div>
       <div className="container-project" ref={project1}>
-        <a href="https://bitblog-puce.vercel.app/" target="_blank"><div className="project"><p>Project One</p></div></a>
-        <a href="https://tip-split-js.vercel.app/" target="_blank"><div className="project"><p>Project Two</p></div></a>
-        <a href="https://recipe-app-react-seven.vercel.app/" target="_blank"><div className="project"><p>Project Three</p></div></a>
-        <a href="https://rick-and-morty-js.vercel.app/" target="_blank"><div className="project"><p>Project Four</p></div></a>
-        <a href="https://tv-shows-react-app.vercel.app/" target="_blank"><div className="project"><p>Project Five</p></div></a>
-        <a href="https://gif-search-react.vercel.app/" target="_blank"><div className="project"><p>Project Six</p></div></a>
+        <a href="https://bitblog-puce.vercel.app/" rel="noreferrer"><div className="project"><p>Project One</p></div></a>
+        <a href="https://tip-split-js.vercel.app/" rel="noreferrer"><div className="project"><p>Project Two</p></div></a>
+        <a href="https://recipe-app-react-seven.vercel.app/" rel="noreferrer"><div className="project"><p>Project Three</p></div></a>
+        <a href="https://rick-and-morty-js.vercel.app/" rel="noreferrer"><div className="project"><p>Project Four</p></div></a>
+        <a href="https://tv-shows-react-app.vercel.app/" rel="noreferrer"><div className="project"><p>Project Five</p></div></a>
+        <a href="https://gif-search-react.vercel.app/" rel="noreferrer"><div className="project"><p>Project Six</p></div></a>
       </div>
     </div>
   )
