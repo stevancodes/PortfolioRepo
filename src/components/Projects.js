3d99e3bc2d1e77978aba20761cf6916ab6a1a7a8
stevanlalovic <stevan.lalovic1999@gmail.com>
2022-07-13 21:54:03 +0200

    link change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,7 @@ function Projects() {
       <Transition timeline={projects} />
       <div className="project-image project-overlay" ref={projectimg}></div>
       <div className="container-project" ref={project1}>
-        <a href="https://bitblog-puce.vercel.app/" rel="noreferrer"><div className="project"><p>Project One</p></div></a>
+        <a href="https://https://bit-blog-react.vercel.app/" rel="noreferrer"><div className="project"><p>Project One</p></div></a>
         <a href="https://tip-split-js.vercel.app/" rel="noreferrer"><div className="project"><p>Project Two</p></div></a>
         <a href="https://recipe-app-react-seven.vercel.app/" rel="noreferrer"><div className="project"><p>Project Three</p></div></a>
         <a href="https://rick-and-morty-js.vercel.app/" rel="noreferrer"><div className="project"><p>Project Four</p></div></a>
